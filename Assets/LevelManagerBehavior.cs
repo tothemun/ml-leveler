a1a0cde24b4abb69d9451d5d04af88862be0f0a3
Jonathan Blair <jon@tothemun.com>
2018-09-10 23:06:29 -0400

    Initial Commit:

---
+++ Assets/LevelManagerBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerBehavior : MonoBehaviour {
+
+    public List<Transform> _fingers = new List<Transform>();
+    public float _level = 0;
+    LineRenderer _line;
+    float LineWidth = 0.1f;
+    public Color UnLevel;
+    public Color Level;
+    public List<MeshRenderer> _aligns = new List<MeshRenderer>();
+    public Transform CenterAlign;
+    void Start() {
+        _line = GetComponent<LineRenderer>();
+    }
+
+
+	void Update () {
+        _level = _fingers[0].position.y - _fingers[1].position.y;
+        bool IsLevel = Mathf.Abs(_level) < 0.0125f;
+        _line.SetPosition(0, _fingers[0].position);
+        _line.SetPosition(1, _fingers[1].position);
+        _line.material.color = IsLevel? Level:UnLevel;
+        foreach(MeshRenderer _m in _aligns) {
+            _m.material.color = IsLevel ? Level : UnLevel;
+        }
+        LineWidth = Mathf.Lerp(LineWidth,IsLevel ? 0.0015f:0.0005f,Time.deltaTime / 0.0625f);
+        _line.startWidth = LineWidth;
+        _line.endWidth = LineWidth;
+
+        transform.position = Vector3.Lerp(_fingers[0].position,_fingers[1].position,0.5f);
+        transform.LookAt(_fingers[0].position);
+        transform.Rotate(0,90,0);
+
+        CenterAlign.localPosition = new Vector3(-_level,CenterAlign.transform.localPosition.y,0);
+	}
+}
---
+++ Assets/Scripts/HandTrackScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class HandTrackScript : MonoBehaviour {
+    #region  Public Variables
+    public enum HandPoses { Ok, Finger, Thumb, OpenHandBack, Fist, NoPose };
+    public HandPoses pose = HandPoses.NoPose;
+    public GameObject LeftIndex, RightIndex;
+    #endregion
+
+    #region Private Variables
+    private MLHandKeyPose[] _gestures;
+    private bool LeftPosing = false;
+    private bool RightPosing = false;
+    #endregion
+
+    #region Unity Methods
+    private void Awake() {
+        MLHands.Start();
+        _gestures = new MLHandKeyPose[5]; 
+        _gestures[0] = MLHandKeyPose.Ok;
+        _gestures[1] = MLHandKeyPose.Finger;
+        _gestures[2] = MLHandKeyPose.OpenHandBack;
+        _gestures[3] = MLHandKeyPose.Fist;
+        _gestures[4] = MLHandKeyPose.Thumb;
+        MLHands.KeyPoseManager.EnableKeyPoses(_gestures, true, false);
+    }
+    private void OnDestroy() {
+        MLHands.Stop();
+    }
+
+    private void Update() {
+        if (GetGesture(MLHands.Left, MLHandKeyPose.Thumb)){
+            ShowPoints(MLHands.Left, LeftIndex, !LeftPosing);
+            LeftPosing = true;
+        }else{
+            LeftPosing = false;
+        }
+
+        if (GetGesture(MLHands.Right, MLHandKeyPose.Thumb)){
+            ShowPoints(MLHands.Right, RightIndex, !RightPosing); 
+            RightPosing = true;
+        }else{
+            RightPosing = false;
+        }
+    }
+    #endregion
+
+    #region Private Methods
+    private void ShowPoints(MLHand hand, GameObject sphere, bool _starting) {
+        Vector3 _pos = hand.Thumb.KeyPoints[0].Position;
+        if(_starting)
+        {
+            sphere.transform.position = _pos;
+        }else{
+            if(Vector3.Distance(sphere.transform.position,_pos) < 0.5f)
+            {
+                sphere.transform.position = Vector3.Lerp(sphere.transform.position,_pos,Time.deltaTime / 0.0625f);
+            }
+        }
+    }
+
+    private bool GetGesture(MLHand hand, MLHandKeyPose type) {
+        if (hand != null) {            
+            if (hand.KeyPose == type) {
+                print(hand.KeyPoseConfidence);
+                if (hand.KeyPoseConfidence > 0.9f) {                       
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    #endregion
+
+}
